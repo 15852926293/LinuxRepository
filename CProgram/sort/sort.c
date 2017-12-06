62025f2c4508208a745d5047ce2195595ebdcf2c
jim-zhang <857204040@qq.com>
2017-12-06 16:33:56 +0800

    Modify add selection sort method in sort.c

--- CProgram/sort/sort.c
+++ CProgram/sort/sort.c
@@ -13,8 +13,8 @@
  * --------------------------------------------------------------------------
  */
 #include<stdio.h>
-int length = 0;
-int data[1000] = {0};
+static int length = 0;
+static int data[1000] = {0};
 void startPrint()
 {
     printf("\nPlease choose the sort method:\ninput 1:Bubble Sort.\ninput 2:Selection Sort.\ninput 3:Both sort methods reslut.\n");
@@ -27,6 +27,7 @@ int getInput()
     while(1)
     {
         if(*input != '1' && *input != '2' && *input != '3')
+        //if(!strcmp(input,"1") && !strcmp(input,"2") && !strcmp(input,"3"))
         {
             printf("Please input 1 , 2 or 3!\n");
             scanf("%s",input);
@@ -54,23 +55,28 @@ void getData()
 
 void Bubble()
 {
-    int i,j,tmp;
+    int data_b[100] = {0};
+    int n,i,j,tmp;
+    for(n = 0; n < length; n++)
+    {
+        data_b[n] = data[n];
+    }
     for(i = 0; i < length-1; i++)
     {
         for(j = 0; j < length-i-1; j++)
         {
-            if(data[j] > data[j+1])
+            if(data_b[j] > data_b[j+1])
             {
-                tmp = data[j];
-                data[j] = data[j+1];
-                data[j+1] = tmp;
+                tmp = data_b[j];
+                data_b[j] = data_b[j+1];
+                data_b[j+1] = tmp;
             }
         }
     }
     printf("Bubble Sort: ");
     for(i = 0; i < length; i++)
     {
-        printf("%d ",data[i]);
+        printf("%d ",data_b[i]);
     }
     printf("\n");
 }
@@ -80,13 +86,58 @@ void Selection()
     /* 
      * to do
      * */
+    int data_s[100] = {0};
+    int n,i,j,tmp,min;
+    for(n = 0; n < length; n++)
+    {
+        data_s[n] = data[n];
+    }
+    for(i = 0; i < length - 1; i++)
+    {
+        min = i;
+        for(j = i+1; j < length; j ++)
+        {
+            if(data_s[min] > data_s[j])
+            {
+                min = j;
+            }
+        }
+        if(min != i)
+        {
+            tmp = data_s[i];
+            data_s[i] = data_s[min];
+            data_s[min] = tmp;
+        }
+    }
+    printf("Select Sort: ");
+    for(i = 0; i < length; i++)
+    {
+        printf("%d ",data_s[i]);
+    }
+    printf("\n");
+
+}
+
+void outPut(int chose)
+{
+    switch (chose)
+    {
+        case 1:Bubble();
+               break;
+        case 2:Selection();
+               break;
+        case 3:Bubble();
+               Selection();
+               break;
+        default:break;
+    }
 }
 
 int main(int argc, char **argv)
 {
     startPrint();
-    int input = getInput();
+    int chose = getInput();
     getData();
-    Bubble();
+    outPut(chose);
     return 0;
 }
